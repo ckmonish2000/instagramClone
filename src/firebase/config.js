5468c660c389fec8eecc2d184408012ba8e635d3
ckmonish2000 <ckmonish2000@gmail.com>
2020-09-20 12:06:42 +0530

    init and config

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyD2Lry51babs4_diX-nlnY24SLXxvmaPuE",
+  authDomain: "firegram-5ad1e.firebaseapp.com",
+  databaseURL: "https://firegram-5ad1e.firebaseio.com",
+  projectId: "firegram-5ad1e",
+  storageBucket: "firegram-5ad1e.appspot.com",
+  messagingSenderId: "666023192877",
+  appId: "1:666023192877:web:8937d6b33073ee4d77cba4",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
